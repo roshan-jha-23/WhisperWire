6190818bf0234e7c38c74cbadd7437a8a706bb9e
roshan-jha-23 <jha062305@gmail.com>
2024-05-04 00:18:32 +0530

    add resend config for sending emails to user

---
+++ src/helper/sendVerificationsEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "",
+      to: email,
+      subject: "WhisperWire || Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: "Verification email sent successfully " };
+  } catch (emailError) {
+    console.log("Error sending Verification email", emailError);
+    return { success: false, message: "Failed to send Verification email" };
+  }
+}
