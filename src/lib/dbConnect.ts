a27b9c20c7f65d93790f55d3a2fe1a24192d267f
roshan-jha-23 <jha062305@gmail.com>
2024-05-04 00:11:47 +0530

    databse conection has beenn setup

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+type ConnectionObject = {
+  isConnected?: number;
+};
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already Connected To Base");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log("MongoDb connected Succesfully");
+  } catch (error) {
+    console.log("DataBase Connection Failed", error);
+    process.exit(1);
+  }
+}
