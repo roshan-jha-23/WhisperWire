73d281bc725805c5a1406fb0385f92caef6ce054
roshan-jha-23 <jha062305@gmail.com>
2024-05-04 12:37:42 +0530

    add route to sign-up and fix bugs

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,11 +8,68 @@ export async function POST(request: Request) {
   await dbConnect();
   try {
     const { username, email, password } = await request.json();
-    const user = UserModel.findOne({ email });
-    if (user) {
-      throw new Error("This email is already in use.");
+    const existingUserVerifiedBYUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedBYUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username Already Taken",
+        },
+        { status: 400 }
+      );
     }
-  } catch (error) {
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const OTP = Math.floor(100000 + Math.random() * 900000).toString();
+    if (existingUserByEmail) {
+      if(existingUserByEmail.isVerified){
+        return Response.json(
+          {
+            success: false,
+            message: "User Alreday registered with this email"
+          },
+          { status: 400 }
+        );
+      }else{
+        const hashedPassword=await bcrypt.hash(password,10)
+        existingUserByEmail.password=hashedPassword;
+        existingUserByEmail.verifyCode=OTP;
+        existingUserByEmail.verifyCodeExpiry=new Date(Date.now()+3600000)
+        await existingUserByEmail.save()
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode: OTP,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+      // Send Verification Email to user's registered email id with the generated OTP
+      const emailResponse = await sendVerificationEmail(email, username, OTP);
+      if (!emailResponse.success) {
+        return Response.json(
+          { success: false, message: emailResponse.message },
+          { status: 500 }
+        );
+      }
+      return Response.json(
+        { success: false, message: emailResponse.message },
+        { status: 500 }
+      );
+    }
+  catch (error) {
     console.log("Error while Resgistering the user");
     return Response.json(
       {
