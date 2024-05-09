043a756b9815c3bf3b7979811645832b7d47f517
roshan-jha-23 <jha062305@gmail.com>
2024-05-09 14:40:05 +0530

    add send message route

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,62 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+
+
+export async function GET(request:Request) {
+    await dbConnect();
+
+     const session = await getServerSession(authOptions);
+
+     const user: User = session?.user as User;
+
+     if (!session || !session.user) {
+       return Response.json(
+         { success: false, message: "Not authenticated" },
+         { status: 401 }
+       );
+     }
+   //bcoz while defining user types we are taking it as a string but while writting aggretation pipelines we need object id so we converted it to mongoose id
+     const userId = new mongoose.Types.ObjectId(user._id)
+    
+     try {
+        const user=await UserModel.aggregate([
+            {
+                $match:{id:userId}
+            },
+            {
+                $unwind:'$messages'
+            },
+            {
+                $sort:{'$messages.createdAt':-1}
+            },
+            {
+                $group:{_id:'$_id',messages:{$push:'$messages'}}
+            }
+        ])
+        if(!user || user.length===0){
+            return Response.json({
+               success:false,
+               message:'User Not found'
+            },{
+                status:404
+            })
+        }
+         return Response.json(
+           {
+             success: true,
+             message: user[0].messages
+           },
+           {
+             status: 200,
+           }
+         );
+     } catch (error) {
+        
+     }
+
+}
